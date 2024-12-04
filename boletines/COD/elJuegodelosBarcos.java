03bdb1919e0038da5c6020128aead9f375fd8e59
Alexandre Sinisterra <sandark673@gmail.com>
2024-12-04 14:22:34 +0100

    boceto

---
+++ boletines/COD/elJuegodelosBarcos.java
@@ -0,0 +1,22 @@
+package COD;
+
+import java.util.Scanner;
+
+public class elJuegodelosBarcos {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[][] matriz = {
+                {0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0},
+        };
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                System.out.print(matriz[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
