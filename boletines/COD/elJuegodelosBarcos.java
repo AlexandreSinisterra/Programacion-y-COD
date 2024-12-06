9ab99d0c44a07378a0f217b2a613337eb9a90ff4
AlexandreSinisterra <sandark673@gmail.com>
2024-12-06 05:38:41 +0100

    ya solo faltan los disparos

--- boletines/COD/elJuegodelosBarcos.java
+++ boletines/COD/elJuegodelosBarcos.java
@@ -7,7 +7,7 @@ public class elJuegodelosBarcos {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         boolean segundobarco=false;
-        int[][] tablero = {//nuestro hermoso tablero vacío
+        int[][][] tablero = {{//nuestro hermoso tablero vacío
                 {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
                 {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
                 {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
@@ -18,12 +18,50 @@ public class elJuegodelosBarcos {
                 {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
                 {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
                 {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        },
+        {//nuestro hermoso tablero vacío
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        },
+        {//nuestro hermoso tablero vacío
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        },
+        {//nuestro hermoso tablero vacío
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        }
         };
         System.out.println("jugador 1, su turno");
         int tipobarco;
         int NB4 = 1;
         int NB3 = 1;
         int NB2 = 2;//cantidad de barcos
+        int vuelta=0;
         do {
             System.out.println("tienes " + NB4 + " barco de 1x4, " + NB3 + " de 1x3 y " + NB2 + " de 1x2");
             System.out.println("1-colocar el barco 1x2");
@@ -60,8 +98,8 @@ public class elJuegodelosBarcos {
                 System.out.println("ya colocaste el máximo numero de barcos tipo 1x4");
                 continue;
             }
-            if (NB2==1) segundobarco=true;
-            meterbarco(tablero,tipobarco+1, segundobarco);
+            vuelta++;
+            meterbarco(tablero,tipobarco+1, vuelta);
         }
         while (NB4 > 0 || NB3 > 0 || NB2 > 0);//se repite hasta que los barcos se acaben
         for (int i = 0; i < tablero[0].length; i++) {
@@ -70,7 +108,7 @@ public class elJuegodelosBarcos {
         System.out.println();
         System.out.println("  +---------------------+");
         int filas = 0;
-        for (int[] ints : tablero) {
+        for (int[] ints : tablero[0]) {
             System.out.print(filas + " | ");
             for (int anInt : ints) {
                 System.out.print(anInt + " ");
@@ -105,7 +143,7 @@ public class elJuegodelosBarcos {
 espacio solo para separar funciones
 
      */
-    public static int[][] meterbarco(int[][] tablero, int tamaño_barco, boolean segundobarco) {
+    public static int[][][] meterbarco(int[][][] tablero, int tamaño_barco, int vuelta) {
         Scanner sc = new Scanner(System.in);
         int tamaño, tamaño_inclinado, guardar_columna1;
         boolean error;
@@ -118,7 +156,7 @@ espacio solo para separar funciones
         System.out.println();
         System.out.println("  +---------------------+");
         int filas = 0;
-        for (int[] ints : tablero) {
+        for (int[] ints : tablero[0]) {
             System.out.print(filas + " | ");
             for (int anInt : ints) {
                 System.out.print(anInt + " ");
@@ -166,23 +204,23 @@ espacio solo para separar funciones
             if (fila1 == fila2 || columna2 == columna1) {
                 for (int i = fila1; i <= fila2; i++) {
                     for (int j = columna1; j <= columna2; j++) {
-                        if (tablero[i][j] != 0) {
+                        if (tablero[i][j][0] != 0) {
                             error = true;//error de casilla ocupada
                             System.out.println("Se estaba construyendo el barco pero choco con otro barco en las coordenadas: " + i + " " + j);
                         }
                     }
                 }
                 if (error) continue;
-                ;
                 for (int i = fila1; i <= fila2; i++) {
                     for (int j = columna1; j <= columna2; j++) {
-                        tablero[i][j] = tamaño_barco;
+                        tablero[i][j][0] = tamaño_barco;
+                        tablero[i][j][vuelta] = tamaño_barco;
                     }
                 }
             } else {
                 guardar_columna1 = columna1;
                 for (int i = fila1; i <= fila2; i++) {
-                    if (tablero[i][columna1] != 0) {
+                    if (tablero[i][columna1][0] != 0) {
                         error = true;//error de casilla ocupada
                         System.out.println("Se estaba construyendo el barco pero choco con otro barco en las coordenadas: " + i + " " + columna1);
                     }
@@ -191,26 +229,13 @@ espacio solo para separar funciones
                 if (error) continue;
                 columna1 = guardar_columna1;//me gustaria mas utilizar en el bucle de abajo esta variable, pero lo pongo asi xq queda mas claro
                 for (int i = fila1; i <= fila2; i++) {
-                    tablero[i][columna1] = tamaño_barco;
+                    tablero[i][columna1][0] = tamaño_barco;
+                    tablero[i][columna1][vuelta] = tamaño_barco;
                     columna1++;
                 }
             }
         }
         while (error);
-        if (segundobarco) {
-            switch (tamaño_barco - 1) {
-                case 1: barco22(fila1,fila2,columna1,columna2); break;
-                case 2: barco3(fila1,fila2,columna1,columna2); break;
-                case 3: barco4(fila1,fila2,columna1,columna2); break;
-            }
-        }
-        else {
-            switch (tamaño_barco - 1) {
-                case 1: barco21(fila1,fila2,columna1,columna2); break;
-                case 2: barco3(fila1,fila2,columna1,columna2); break;
-                case 3: barco4(fila1,fila2,columna1,columna2); break;
-            }
-        }
         return tablero;
     }
 
@@ -221,7 +246,7 @@ espacio solo para separar funciones
 espacio solo para separar funciones
 
      */
-    public static String[][] disparobarco(String[][] tablero2, int[][] tablero) {
+    public static String[][] disparobarco(String[][] tablero2, int[][][] tablero) {
         Scanner sc = new Scanner(System.in);
         System.out.println("este es tu tablero:");
         System.out.print("    ");
@@ -245,239 +270,14 @@ espacio solo para separar funciones
         int fila = sc.nextInt();
         System.out.println("columna:");
         int columna = sc.nextInt();
-        if (tablero[fila][columna]==0) System.out.println("agua");
+        if (tablero[fila][columna][0]==0){
+            System.out.println("agua");
+            tablero2[fila][columna]="O";
+        }
+        else {
+            System.out.println("tocado");
+            tablero2[fila][columna]="X";
+        }
         return tablero2;
-    }
-/*
-
-espacio solo para separar funciones
-
-     */
-
-    public static String barco3(int fila1, int fila2, int columna1, int columna2){
-        String fila1S="",fila2S="",columna1S="",columna2S="";
-        switch (fila1){
-            case 0: fila1S="q"; break;
-            case 1: fila1S="w"; break;
-            case 2: fila1S="e"; break;
-            case 3: fila1S="r"; break;
-            case 4: fila1S="t"; break;
-            case 5: fila1S="y"; break;
-            case 6: fila1S="u"; break;
-            case 7: fila1S="i"; break;
-            case 8: fila1S="o"; break;
-            case 9: fila1S="p"; break;
-        }
-        switch (columna1){
-            case 0: columna1S="q"; break;
-            case 1: columna1S="w"; break;
-            case 2: columna1S="e"; break;
-            case 3: columna1S="r"; break;
-            case 4: columna1S="t"; break;
-            case 5: columna1S="y"; break;
-            case 6: columna1S="u"; break;
-            case 7: columna1S="i"; break;
-            case 8: columna1S="o"; break;
-            case 9: columna1S="p"; break;
-        }
-        switch (fila2){
-            case 0: fila2S="q"; break;
-            case 1: fila2S="w"; break;
-            case 2: fila2S="e"; break;
-            case 3: fila2S="r"; break;
-            case 4: fila2S="t"; break;
-            case 5: fila2S="y"; break;
-            case 6: fila2S="u"; break;
-            case 7: fila2S="i"; break;
-            case 8: fila2S="o"; break;
-            case 9: fila2S="p"; break;
-        }
-        switch (columna2){
-            case 0: columna2S="q"; break;
-            case 1: columna2S="w"; break;
-            case 2: columna2S="e"; break;
-            case 3: columna2S="r"; break;
-            case 4: columna2S="t"; break;
-            case 5: columna2S="y"; break;
-            case 6: columna2S="u"; break;
-            case 7: columna2S="i"; break;
-            case 8: columna2S="o"; break;
-            case 9: columna2S="p"; break;
-        }
-        return fila1S+columna1S+fila2S+columna2S;
-    }
-    /*
-
-espacio solo para separar funciones
-
-     */
-
-    public static String barco21(int fila1, int fila2, int columna1, int columna2){
-        String fila1S="",fila2S="",columna1S="",columna2S="";
-        switch (fila1){
-            case 0: fila1S="q"; break;
-            case 1: fila1S="w"; break;
-            case 2: fila1S="e"; break;
-            case 3: fila1S="r"; break;
-            case 4: fila1S="t"; break;
-            case 5: fila1S="y"; break;
-            case 6: fila1S="u"; break;
-            case 7: fila1S="i"; break;
-            case 8: fila1S="o"; break;
-            case 9: fila1S="p"; break;
-        }
-        switch (columna1){
-            case 0: columna1S="q"; break;
-            case 1: columna1S="w"; break;
-            case 2: columna1S="e"; break;
-            case 3: columna1S="r"; break;
-            case 4: columna1S="t"; break;
-            case 5: columna1S="y"; break;
-            case 6: columna1S="u"; break;
-            case 7: columna1S="i"; break;
-            case 8: columna1S="o"; break;
-            case 9: columna1S="p"; break;
-        }
-        switch (fila2){
-            case 0: fila2S="q"; break;
-            case 1: fila2S="w"; break;
-            case 2: fila2S="e"; break;
-            case 3: fila2S="r"; break;
-            case 4: fila2S="t"; break;
-            case 5: fila2S="y"; break;
-            case 6: fila2S="u"; break;
-            case 7: fila2S="i"; break;
-            case 8: fila2S="o"; break;
-            case 9: fila2S="p"; break;
-        }
-        switch (columna2){
-            case 0: columna2S="q"; break;
-            case 1: columna2S="w"; break;
-            case 2: columna2S="e"; break;
-            case 3: columna2S="r"; break;
-            case 4: columna2S="t"; break;
-            case 5: columna2S="y"; break;
-            case 6: columna2S="u"; break;
-            case 7: columna2S="i"; break;
-            case 8: columna2S="o"; break;
-            case 9: columna2S="p"; break;
-        }
-        return fila1S+columna1S+fila2S+columna2S;
-    }
-    /*
-
-espacio solo para separar funciones
-
-     */
-
-    public static String barco22(int fila1, int fila2, int columna1, int columna2){
-        String fila1S="",fila2S="",columna1S="",columna2S="";
-        switch (fila1){
-            case 0: fila1S="q"; break;
-            case 1: fila1S="w"; break;
-            case 2: fila1S="e"; break;
-            case 3: fila1S="r"; break;
-            case 4: fila1S="t"; break;
-            case 5: fila1S="y"; break;
-            case 6: fila1S="u"; break;
-            case 7: fila1S="i"; break;
-            case 8: fila1S="o"; break;
-            case 9: fila1S="p"; break;
-        }
-        switch (columna1){
-            case 0: columna1S="q"; break;
-            case 1: columna1S="w"; break;
-            case 2: columna1S="e"; break;
-            case 3: columna1S="r"; break;
-            case 4: columna1S="t"; break;
-            case 5: columna1S="y"; break;
-            case 6: columna1S="u"; break;
-            case 7: columna1S="i"; break;
-            case 8: columna1S="o"; break;
-            case 9: columna1S="p"; break;
-        }
-        switch (fila2){
-            case 0: fila2S="q"; break;
-            case 1: fila2S="w"; break;
-            case 2: fila2S="e"; break;
-            case 3: fila2S="r"; break;
-            case 4: fila2S="t"; break;
-            case 5: fila2S="y"; break;
-            case 6: fila2S="u"; break;
-            case 7: fila2S="i"; break;
-            case 8: fila2S="o"; break;
-            case 9: fila2S="p"; break;
-        }
-        switch (columna2){
-            case 0: columna2S="q"; break;
-            case 1: columna2S="w"; break;
-            case 2: columna2S="e"; break;
-            case 3: columna2S="r"; break;
-            case 4: columna2S="t"; break;
-            case 5: columna2S="y"; break;
-            case 6: columna2S="u"; break;
-            case 7: columna2S="i"; break;
-            case 8: columna2S="o"; break;
-            case 9: columna2S="p"; break;
-        }
-        return fila1S+columna1S+fila2S+columna2S;
-    }
-    /*
-
-espacio solo para separar funciones
-
-     */
-
-    public static String barco4(int fila1, int fila2, int columna1, int columna2){
-        String fila1S="",fila2S="",columna1S="",columna2S="";
-        switch (fila1){
-            case 0: fila1S="q"; break;
-            case 1: fila1S="w"; break;
-            case 2: fila1S="e"; break;
-            case 3: fila1S="r"; break;
-            case 4: fila1S="t"; break;
-            case 5: fila1S="y"; break;
-            case 6: fila1S="u"; break;
-            case 7: fila1S="i"; break;
-            case 8: fila1S="o"; break;
-            case 9: fila1S="p"; break;
-        }
-        switch (columna1){
-            case 0: columna1S="q"; break;
-            case 1: columna1S="w"; break;
-            case 2: columna1S="e"; break;
-            case 3: columna1S="r"; break;
-            case 4: columna1S="t"; break;
-            case 5: columna1S="y"; break;
-            case 6: columna1S="u"; break;
-            case 7: columna1S="i"; break;
-            case 8: columna1S="o"; break;
-            case 9: columna1S="p"; break;
-        }
-        switch (fila2){
-            case 0: fila2S="q"; break;
-            case 1: fila2S="w"; break;
-            case 2: fila2S="e"; break;
-            case 3: fila2S="r"; break;
-            case 4: fila2S="t"; break;
-            case 5: fila2S="y"; break;
-            case 6: fila2S="u"; break;
-            case 7: fila2S="i"; break;
-            case 8: fila2S="o"; break;
-            case 9: fila2S="p"; break;
-        }
-        switch (columna2){
-            case 0: columna2S="q"; break;
-            case 1: columna2S="w"; break;
-            case 2: columna2S="e"; break;
-            case 3: columna2S="r"; break;
-            case 4: columna2S="t"; break;
-            case 5: columna2S="y"; break;
-            case 6: columna2S="u"; break;
-            case 7: columna2S="i"; break;
-            case 8: columna2S="o"; break;
-            case 9: columna2S="p"; break;
-        }
-        return fila1S+columna1S+fila2S+columna2S;
     }
 }
