4de84abbcef81b0bad05842f34ec771550051d48
AlexandreSinisterra <sandark673@gmail.com>
2024-12-06 01:09:16 +0100

    posibles errores al introducir las coordenadas del barco + prueba de do while con continue

--- boletines/COD/elJuegodelosBarcos.java
+++ boletines/COD/elJuegodelosBarcos.java
@@ -6,11 +6,16 @@ public class elJuegodelosBarcos {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int[][] tablero = {//nuestro hermoso tablero vacío
-                {0, 0, 0, 0, 0},
-                {0, 0, 0, 0, 0},
-                {0, 0, 0, 0, 0},
-                {0, 0, 0, 0, 0},
-                {0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
         };
         int tipobarco; int NB4=1; int NB3=1; int NB2=2;//cantidad de barcos
         do {
@@ -57,9 +62,11 @@ public class elJuegodelosBarcos {
         }
         sc.close();
     }
-    public static int[][] meterbarco(int [][] tablero, int barco){
+    public static int[][] meterbarco(int [][] tablero, int tamaño_barco){
         Scanner sc = new Scanner(System.in);
-        int tamaño_barco=barco;
+        int tamaño;
+        boolean error;
+        int cambio;
         System.out.println("este es tu tablero:");
         for (int[] ints : tablero) {
             for (int anInt : ints) {
@@ -68,16 +75,50 @@ public class elJuegodelosBarcos {
             System.out.println();
         }
         System.out.println("donde quieres colocar tu barco, pon las coordenadas de las 2 puntas del barco, solo horizontal y vertical se puede colocar");
-        System.out.println("fila:");
-        int fila1= sc.nextInt();
-        System.out.println("columna:");
-        int columna1= sc.nextInt();
-        System.out.println("ahora dime las de la otra punta:");
-        System.out.println("fila:");
-        int fila2= sc.nextInt();
-        System.out.println("columna:");
-        int columna3= sc.nextInt();
-
+        do {
+            error=false;
+            System.out.println("fila:");
+            int fila1 = sc.nextInt();
+            System.out.println("columna:");
+            int columna1 = sc.nextInt();
+            System.out.println("ahora dime las de la otra punta:");
+            System.out.println("fila:");
+            int fila2 = sc.nextInt();
+            System.out.println("columna:");
+            int columna2 = sc.nextInt();
+            if (fila1<0||fila1>9||fila2<0||fila2>9||columna1<0||columna1>9||columna2<0||columna2>9){
+                error=true;//error de fuera de tablero
+                System.out.println("casilla inválida, tiene que estar dentro del tablero, los valores son del 0 al 9.");
+                continue;
+            }
+            if (fila2 < fila1) {//cambio para que la fila 1 siempre sea la que tiene un valor menor
+                cambio = fila2;
+                fila2 = fila1;
+                fila1 = cambio;
+            }
+            if (columna2 < columna1) {//lo mismo que el cambio de arriba pero con columnas
+                cambio = columna2;
+                columna2 = columna1;
+                columna1 = cambio;
+            }
+            tamaño=fila2-fila1+columna2-columna1;
+            if (tamaño!=tamaño_barco){
+                error=true;//error de tamaño de barco
+                System.out.println("las coordenadas tienen un tamaño diferente al barco.");
+                System.out.println("intentelo de nuevo");
+                continue;
+            }
+            for (int i = fila1; i <= fila2; i++) {
+                for (int j = columna1; j <= columna2; j++) {
+                    if (tablero[i][j] != 0) {
+                        error = true;//error de casilla ocupada
+                        System.out.println("Se estaba construyendo el barco pero choco con otro barco en las coordenadas: "+i+" "+j);
+                        System.out.println("por favor, introduzca otras coordenadas");
+                    }
+                }
+            }
+        }
+        while(error);
 
         return tablero;
     }
