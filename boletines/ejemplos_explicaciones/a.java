6c9aff72d4cb58cf8377e799ea6867c0a986bb30
Alexandre Sinisterra <sandark673@gmail.com>
2025-01-08 09:23:46 +0100

    a

---
+++ boletines/ejemplos_explicaciones/a.java
@@ -0,0 +1,12 @@
+package ejemplos_explicaciones;
+
+public class a {
+    public static void main(String args[]){
+        char caracter;
+        caracter = 'a';
+        caracter = 9825;//hexadecimal b
+        caracter = '\u2661';//unicode a
+        caracter = 936;//hexadecimal letra griega
+        System.out.println(caracter);
+    }
+}
