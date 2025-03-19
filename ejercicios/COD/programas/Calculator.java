321cef7095a8c77fa524ea7afd349071b4813af4
Alexandre Sinisterra <sandark673@gmail.com>
2025-03-19 12:28:59 +0100

    en calculatorTest tuve que colocar public al principio

--- ejercicios/COD/programas/Calculator.java
+++ ejercicios/COD/programas/Calculator.java
@@ -1,11 +1,7 @@
 package COD.programas;
 
 public class Calculator {
-
-    // Método para sumar dos números
     public int add(int a, int b) {
         return a + b;
     }
-
-    // Puedes agregar más métodos si es necesario
 }
